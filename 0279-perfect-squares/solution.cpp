17f37c10c531e54b77db32d6ebcecccb5447c192
dev-rjt <rajatgupta15g@gmail.com>
2025-11-15 16:18:44 +0000

    Sync LeetCode submission Runtime - 75 ms (40.64%), Memory - 13.9 MB (31.91%)

--- 0279-perfect-squares/solution.cpp
+++ 0279-perfect-squares/solution.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
 
-    int solve(int n,vector <int> &dp) {
+    int solve(int n,vector<int> &dp) {
+        if(n<0) return INT_MAX;
         if(n==0) return 0;
-        if(n==1) return 1;
 
         if(dp[n]!=-1) return dp[n];
 
-        int ans = n;
+        int mini = INT_MAX;
 
-        for(int i=1;i*i<=n;i++) {
-            ans = min(ans,solve(n-i*i,dp)+1);
+        for(int j=1;j*j<=n;j++){
+            int ans = 1 + solve(n-j*j,dp);
+            mini = min(mini,ans);
         }
 
-        dp[n] = ans;
-        return ans;
+        dp[n] = mini;
+        return mini;
     }
+
     int numSquares(int n) {
-        vector <int> dp(n+1,-1);
-        return solve(n,dp);
+        vector<int> dp(n+1,-1);
+        int x = solve(n,dp);
+
+        if(x==INT_MAX) return -1;
+        else return x;
     }
 };
