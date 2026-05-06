25ad31acfea296d2ab1647eac44d1621bd358944
dev-rjt <rajatgupta15g@gmail.com>
2026-05-06 15:15:04 +0000

    Sync LeetCode submission Runtime - 960 ms (89.75%), Memory - 0.0B (100.00%)

---
+++ 1415-students-and-examinations/solution.sql
@@ -0,0 +1,1 @@
+SELECT S.student_id,S.student_name,sub.subject_name,COUNT(e.subject_name) AS attended_exams FROM Students AS S CROSS JOIN Subjects AS sub LEFT JOIN Examinations AS e ON s.student_id = e.student_id AND sub.subject_name = e.subject_name GROUP BY s.student_id ,s.student_name,sub.subject_name ORDER BY s.student_id,sub.subject_name
