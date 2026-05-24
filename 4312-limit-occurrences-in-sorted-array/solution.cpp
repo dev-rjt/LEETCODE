c7ebec72167858f92947f7d5bf779ac2770462ea
dev-rjt <rajatgupta15g@gmail.com>
2026-05-24 16:53:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 34 MB (9.11%)

---
+++ 4312-limit-occurrences-in-sorted-array/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> limitOccurrences(vector<int>& nums, int k) {
+        unordered_map <int,int> freq;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            if(freq[nums[i]]<k){
+                ans.push_back(nums[i]);
+                freq[nums[i]]++;
+            }
+        }
+        return ans;
+    }
+};
