7af4d332f8632add3c41deb7235e8de442fe69de
dev-rjt <rajatgupta15g@gmail.com>
2026-05-29 19:08:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9 MB (37.83%)

--- 0020-valid-parentheses/solution.cpp
+++ 0020-valid-parentheses/solution.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     bool isValid(string s) {
-        int size = s.length();
-
-        stack <char> s1;
-
-
-        for(int i=0;i<size;i++){
-            if(s[i]=='(' || s[i]=='{' || s[i]=='[') s1.push(s[i]);
-
-            if(s[i]==')' || s[i]=='}' || s[i]==']') {
-                if(s1.size()<1) return false;
-
-                else if(s[i]==')' && s1.top() == '('|| s[i]=='}' && s1.top() == '{' || s[i]==']' && s1.top() == '[') {
-                    s1.pop();
-                    continue;
-                }
-
-                else return false;
+        stack <char> st;
+        int poss = 1;
+        
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='{' || s[i]=='[' || s[i]=='('){
+                st.push(s[i]);
+                continue;
             }
-
+            
+            if(!st.empty() && s[i]==')' && st.top()=='(') st.pop();
+            else if(!st.empty() && s[i]=='}' && st.top()=='{') st.pop();
+            else if(!st.empty() && s[i]==']' && st.top()=='[') st.pop();
+            else{
+                poss = 0;
+                break;
+            }
+            
         }
-
-        if(s1.size()>0) return false;
-
-        else return true;
+        
+        if(st.empty()&&poss) return true;
+        else return false;
     }
 };
