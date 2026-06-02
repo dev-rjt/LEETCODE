d4489e39c8fcfc009c727d50ab8c7d02698455ae
dev-rjt <rajatgupta15g@gmail.com>
2026-06-02 11:47:35 +0000

    Sync LeetCode submission Runtime - 2 ms (38.02%), Memory - 15.8 MB (5.87%)

--- 0078-subsets/solution.cpp
+++ 0078-subsets/solution.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
+    void solve(vector<int> &nums,int i,vector<int> res,vector<vector<int>> &ans){
+        if(i>=nums.size()){
+            ans.push_back(res);
+            return;
+        };
 
-    void include(vector<int> &nums,vector<int> &subset,int index,int len,int n,vector<vector<int>> &a) {
-        if(index==n) {
-            a.push_back(subset);
-            return; 
-        }
+        solve(nums,i+1,res,ans);
 
-        subset.push_back(nums[index]);
-        include(nums,subset,index+1,len+1,n,a);
-        subset.pop_back();
-        include(nums,subset,index+1,len,n,a);
+        res.push_back(nums[i]);
+        solve(nums,i+1,res,ans);
+        res.pop_back();
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> subset;
-        vector<vector<int>> a;
-        include(nums,subset,0,0,n,a);
-
-        return a;
+        vector<vector<int>> ans;
+        solve(nums,0,{},ans);
+        return ans;
     }
 };
