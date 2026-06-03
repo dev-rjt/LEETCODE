593cc3d71f9b8a29374325b413f4951f7d75d270
dev-rjt <rajatgupta15g@gmail.com>
2026-06-03 08:43:39 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.9 MB (25.74%)

---
+++ 0090-subsets-ii/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void solve(vector<int> &nums,int i,vector<int> res,vector<vector<int>> &ans){
+        ans.push_back(res);
+
+        for(int j=i;j<nums.size();j++){
+
+            if(j>i && nums[j]==nums[j-1]){
+                continue;
+            }
+            
+            res.push_back(nums[j]);
+            solve(nums,j+1,res,ans);
+            res.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        solve(nums,0,{},ans);
+        return ans;
+    }
+};
