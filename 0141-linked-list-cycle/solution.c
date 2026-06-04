ac1c607c3713c671701e5be8c373cd3f0275ae9d
dev-rjt <rajatgupta15g@gmail.com>
2026-06-04 19:27:42 +0000

    Sync LeetCode submission Runtime - 9 ms (79.68%), Memory - 11.8 MB (85.32%)

--- 0141-linked-list-cycle/solution.c
+++ 0141-linked-list-cycle/solution.c
@@ -6,20 +6,15 @@
  * };
  */
 bool hasCycle(struct ListNode *head) {
-    if (head == NULL || head->next == NULL)
-        return false;
+    struct ListNode *first = head;
+    struct ListNode *second = head;
 
-    struct ListNode *slow = head;
-    struct ListNode *fast = head;
+    if(head==NULL) return false;
 
-    while (fast != NULL && fast->next != NULL) {
-        slow = slow->next;
-        fast = fast->next->next;
-
-        if (slow == fast)
-            return true;
+    while(second->next&& second->next->next ){
+        second = second->next->next;
+        if(second==first) return true;
+        first = first->next;
     }
-
     return false;
 }
-
