fe7e18d6e2bc2feb61f099990376f5c6927f56fb
dev-rjt <rajatgupta15g@gmail.com>
2026-06-09 07:51:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 103.6 MB (67.68%)

---
+++ 4005-maximum-total-subarray-value-i/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long maxTotalValue(vector<int>& nums, int k) {
+        long long ans = 0;
+        int maxi = INT_MIN;
+        int mini = INT_MAX;
+
+        for(int i=0;i<nums.size();i++){
+            maxi = max(maxi,nums[i]);
+            mini = min(mini,nums[i]);
+        }
+
+        ans = (long long)(maxi-mini)*k;
+        return ans;
+    }
+};
