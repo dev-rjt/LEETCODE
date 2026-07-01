b94293035b19c6e5154145eaf9e264a2687a6789
dev-rjt <rajatgupta15g@gmail.com>
2026-07-01 12:27:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 34.3 MB (51.30%)

--- 0450-delete-node-in-a-bst/solution.cpp
+++ 0450-delete-node-in-a-bst/solution.cpp
@@ -11,55 +11,24 @@
  */
 class Solution {
 public:
-
-    TreeNode *findmini(TreeNode* root){
-        if(root->left==NULL) return root;
-
-        return findmini(root->left);
-
-    }
-
     TreeNode* deleteNode(TreeNode* root, int key) {
         if(root==NULL) return NULL;
 
-        if(root->val==key){
-            //0 child
-            if(root->right==NULL && root->left==NULL){
-                delete root;
-                return NULL;
-            }
-
-            //1 child
-            if(root->right!=NULL && root->left==NULL){
-                TreeNode *temp = root->right;
-                delete root;
-                return temp;
-            }
-
-            if(root->right==NULL && root->left!=NULL){
-                TreeNode *temp = root->left;
-                delete root;
-                return temp;
-            }
-
-            //2 child
-
-            if(root->left!=NULL && root->right!=NULL){
-                TreeNode *temp = findmini(root->right);
-                root->val = temp->val;
-                root->right = deleteNode(root->right,temp->val);
-                return root;
-            }
-        }
-
-        else if(root->val>key){
-            root->left = deleteNode(root->left,key);
-            return root;
-        }
+        if(root->val<key) root->right = deleteNode(root->right,key);
+        else if(root->val>key) root->left = deleteNode(root->left,key);
 
         else{
-            root->right = deleteNode(root->right,key);
-            return root;
+            if(!root->left && !root->right){
+                return NULL;
+            }
+            else if(root->left && !root->right) return root->left;
+            else if(!root->left && root->right) return root->right;
+            else{
+                TreeNode* temp = root->left;
+                while(temp->right) temp = temp->right;
+                temp->right = root->right;
+                return root->left;
+            }
         }
         return root;
     }
