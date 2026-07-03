d40e6bceaa915109555683c1e89faed6d2d8c3c2
dev-rjt <rajatgupta15g@gmail.com>
2026-07-03 10:25:58 +0000

    Sync LeetCode submission Runtime - 7 ms (99.81%), Memory - 17.9 MB (83.72%)

--- 0322-coin-change/solution.cpp
+++ 0322-coin-change/solution.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
-    int solve2(vector<int> coins,int amount,vector<int> &dp) {
-        if(amount<0) return INT_MAX;
-        if(amount==0) return 0;
-
-        int mini = INT_MAX;
-
-        if(dp[amount]!=-1) return dp[amount];
-
-        for(int i=0;i<coins.size();i++) {
-            int ans = solve2(coins,amount-coins[i],dp);
-            if(ans!=INT_MAX) mini = min(mini,ans+1);
-        }
-        dp[amount] = mini;
-        return dp[amount];
-    }
-
-    
     int coinChange(vector<int>& coins, int amount) {
+        
+        vector<int> dp(amount+1,amount+1);
+        dp[0] = 0;
 
-        vector <int> dp(amount+1,-1);
-        solve2(coins,amount,dp);
-        int x = solve2(coins,amount,dp);
-        if(x==INT_MAX) return -1;
-        else return x;
+        for(int i=1;i<=amount;++i){
+            for(int coin : coins){
+                if(i-coin >=0){
+                    dp[i] = min(dp[i],1+dp[i-coin]);
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 };
